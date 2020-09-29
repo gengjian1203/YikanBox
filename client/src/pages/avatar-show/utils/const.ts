1fd447697724ffc8228430cb6b8157f42ca982a2
gengjian1203 <gengjian1203@foxmail.com>
2020-09-29 18:37:12 +0800

    feat(头像秀):新增头像秀的贴图资源

--- client/src/pages/avatar-show/utils/const.ts
+++ client/src/pages/avatar-show/utils/const.ts
@@ -1,5 +1,22 @@
 import Config from '@/config/index'
 
+// Draw
+const strUrlJewelryZhongqiu00 =
+	Config.cloudPath + '/avatar/jewelry/zhongqiu_00.png'
+const strUrlJewelryZhongqiu01 =
+	Config.cloudPath + '/avatar/jewelry/zhongqiu_01.png'
+
+const strUrlJewelryGuoqing00 =
+	Config.cloudPath + '/avatar/jewelry/guoqing_00.png'
+const strUrlJewelryGuoqing01 =
+	Config.cloudPath + '/avatar/jewelry/guoqing_01.png'
+const strUrlJewelryGuoqing02 =
+	Config.cloudPath + '/avatar/jewelry/guoqing_02.png'
+const strUrlJewelryGuoqing03 =
+	Config.cloudPath + '/avatar/jewelry/guoqing_03.png'
+const strUrlJewelryGuoqing04 =
+	Config.cloudPath + '/avatar/jewelry/guoqing_04.png'
+
 const strUrlJewelryHongdian = Config.cloudPath + '/avatar/jewelry/hongdian.png'
 const strUrlJewelryShanchu = Config.cloudPath + '/avatar/jewelry/shanchu.png'
 const strUrlJewelryShengdanlaoren =
@@ -9,6 +26,19 @@ const strUrlJewelryShengdanmao =
 const strUrlJewelryShengdanshu =
 	Config.cloudPath + '/avatar/jewelry/shengdanshu.png'
 const strUrlJewelryXueren = Config.cloudPath + '/avatar/jewelry/xueren.png'
+
+// EG
+
+const strUrlJewelryZhongqiu00EG =
+	Config.cloudPath + '/avatar/eg/zhongqiu_00.jpg'
+const strUrlJewelryZhongqiu01EG =
+	Config.cloudPath + '/avatar/eg/zhongqiu_01.jpg'
+
+const strUrlJewelryGuoqing00EG = Config.cloudPath + '/avatar/eg/guoqing_00.jpg'
+const strUrlJewelryGuoqing01EG = Config.cloudPath + '/avatar/eg/guoqing_01.jpg'
+const strUrlJewelryGuoqing02EG = Config.cloudPath + '/avatar/eg/guoqing_02.jpg'
+const strUrlJewelryGuoqing03EG = Config.cloudPath + '/avatar/eg/guoqing_03.jpg'
+const strUrlJewelryGuoqing04EG = Config.cloudPath + '/avatar/eg/guoqing_04.jpg'
 
 const strUrlJewelryHongdianEG = Config.cloudPath + '/avatar/eg/hongdian.jpg'
 const strUrlJewelryShanchuEG = Config.cloudPath + '/avatar/eg/shanchu.jpg'
@@ -27,6 +57,93 @@ export const BORDER_BUTTON_RADIUS = BORDER_BUTTON_SIZE / 2
 
 // 饰品列表
 export const arrJewelryList = [
+	{
+		title: '中秋',
+		list: [
+			{
+				type: 'IMAGE',
+				value: strUrlJewelryZhongqiu00,
+				valueEG: strUrlJewelryZhongqiu00EG,
+				rect: {
+					x: CANVAS_WIDTH - 100,
+					y: 0,
+					width: 100,
+					height: 120,
+				},
+			},
+			{
+				type: 'IMAGE',
+				value: strUrlJewelryZhongqiu01,
+				valueEG: strUrlJewelryZhongqiu01EG,
+				rect: {
+					x: CANVAS_WIDTH - 120,
+					y: CANVAS_HEIGHT - 120,
+					width: 120,
+					height: 120,
+				},
+			},
+		],
+	},
+	{
+		title: '国庆',
+		list: [
+			{
+				type: 'IMAGE',
+				value: strUrlJewelryGuoqing00,
+				valueEG: strUrlJewelryGuoqing00EG,
+				rect: {
+					x: 0,
+					y: 0,
+					width: CANVAS_WIDTH,
+					height: Math.floor((CANVAS_WIDTH / 300) * 100),
+				},
+			},
+			{
+				type: 'IMAGE',
+				value: strUrlJewelryGuoqing01,
+				valueEG: strUrlJewelryGuoqing01EG,
+				rect: {
+					x: 0,
+					y: CANVAS_HEIGHT - Math.floor((CANVAS_WIDTH / 300) * 116),
+					width: CANVAS_WIDTH,
+					height: Math.floor((CANVAS_WIDTH / 300) * 116),
+				},
+			},
+			{
+				type: 'IMAGE',
+				value: strUrlJewelryGuoqing02,
+				valueEG: strUrlJewelryGuoqing02EG,
+				rect: {
+					x: 0,
+					y: CANVAS_HEIGHT - Math.floor((CANVAS_WIDTH / 300) * 86),
+					width: CANVAS_WIDTH,
+					height: Math.floor((CANVAS_WIDTH / 300) * 86),
+				},
+			},
+			{
+				type: 'IMAGE',
+				value: strUrlJewelryGuoqing03,
+				valueEG: strUrlJewelryGuoqing03EG,
+				rect: {
+					x: CANVAS_WIDTH - 120,
+					y: CANVAS_HEIGHT - 120,
+					width: 120,
+					height: 120,
+				},
+			},
+			{
+				type: 'IMAGE',
+				value: strUrlJewelryGuoqing04,
+				valueEG: strUrlJewelryGuoqing04EG,
+				rect: {
+					x: CANVAS_WIDTH - 120,
+					y: CANVAS_HEIGHT - 109,
+					width: 120,
+					height: 109,
+				},
+			},
+		],
+	},
 	{
 		title: '贴纸',
 		list: [
